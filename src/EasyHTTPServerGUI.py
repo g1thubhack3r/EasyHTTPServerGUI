adb93a2d70351d61fbfb460d982649b352cdeabd
g1thubhack3r <125267748+g1thubhack3r@users.noreply.github.com>
2023-05-02 11:45:55 +0800

    Create EasyHTTPServerGUI.py

---
+++ src/EasyHTTPServerGUI.py
@@ -0,0 +1,39 @@
+import tkinter as tk
+import tkinter.messagebox as msgbox
+import flask
+root = tk.Tk()
+root.title("EasyHTTPServerGUI")
+description = tk.Label(root, text="""
+Enter the address where the HTTP server run in the first entry,
+and the port in the second entry,
+the path in the third,
+and the content in the fourth.
+""")
+description.pack()
+host = tk.Entry(root)
+host.pack()
+hostvar = tk.StringVar(host, "127.0.0.1")
+host['textvariable'] = hostvar
+port = tk.Entry(root)
+portvar = tk.StringVar(port, "80")
+port['textvariable'] = portvar
+port.pack()
+path = tk.Entry(root)
+pathvar = tk.StringVar(path, "/")
+path['textvariable'] = pathvar
+path.pack()
+content = tk.Text(root)
+content.pack()
+def serverrun(host, port, path, content):
+    app = flask.Flask(__name__)
+    @app.route(path)
+    def index():
+        nonlocal content
+        return content
+    try:
+        app.run(host=host, port=port, debug=False)
+    except Exception as e:
+        msgbox.showerror("Error", "There is an error in the HTTP server: " + str(e))
+runbutton = tk.Button(root, text="Run", command=(lambda: serverrun(hostvar.get(), int(portvar.get()), path.get(), content.get(1.0, tk.END))))
+runbutton.pack()
+root.mainloop()
